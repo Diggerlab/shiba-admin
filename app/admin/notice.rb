d5a902fc32df6b432b76c790b17ac44febef165a
Daneil Wei <daneilmu1@gmail.com>
2014-09-04 14:41:06 +0800

    change the notice column

--- app/admin/notice.rb
+++ app/admin/notice.rb
@@ -1,24 +1,20 @@
 ActiveAdmin.register Notice do
  menu label: 'Notices', parent: 'Operation'
-  index  do  
-    column "活动设置时间",:set_time                  
-    column "結束時間",:end_at     
-    column "显示时间",:show_time                 
+  index  do                   
+    column "結束時間",:closed_at     
+    column "显示时间",:published_at                 
     default_actions    
   end         
            
   form do |f|
-    f.inputs "活動显示时间" do
-      f.input :show_time
-    end
     f.inputs "活动标题" do
       f.input :title
     end
-    f.inputs "设置时间" do
-      f.input :set_time
+    f.inputs "活動显示时间" do
+      f.input :published_at
     end
     f.inputs "結束時間" do
-      f.input :end_at, value: (Time.now + 3600)
+      f.input :closed_at, value: (Time.now + 3600)
     end
     f.inputs "content_en" do
       f.kindeditor :content_en, :owner_id => current_admin_user.id, filterMode: false
